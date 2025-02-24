06eddac5b46a79a73fa025f2f12b62043e3dff29
johan-Andersen <joan0005@stud.kea.dk>
2025-02-24 14:08:02 +0100

    Initial commit

---
+++ src/main/java/org/example/hellogithubactionsdemo/HelloGithubActionsDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.hellogithubactionsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGithubActionsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloGithubActionsDemoApplication.class, args);
+    }
+
+}
